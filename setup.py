be3e8ce7242c0329cabc4f0419975510369bae04
jrwhite <jrwhite20@gmail.com>
2017-09-26 11:36:52 -0500

    v0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "neurobytes",
-    version = "0.1",
+    version = "0.11",
     description = "NeuroBytes Python Module and CLI",
     url = "https://github.com/NeuroTinker/neurobytes-python",
     author = "Jarod White",
